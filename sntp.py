ff49fdf3f5263b28018aa69e9ae7b4dde453715f
rozuur <rozuur@gmail.com>
2011-06-15 17:40:38 +0530

    current time implemented from sntp servers

--- sntp.py
+++ sntp.py
@@ -1,17 +1,35 @@
 import socket
 import struct
-import sys
 import time
 
-TIME1970 = 2208988800L      # Thanks to F.Lundh
 
-client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
-data = '\x1b' + 47 * '\0'
-client.sendto( data, ( sys.argv[1], 123 ))
-data, address = client.recvfrom( 1024 )
-if data:
-    print 'Response received from:', address
-    t = struct.unpack( '!12I', data )[10]
-    t -= TIME1970
-    print '\tTime=%s' % time.ctime(t)
+
+#        t -= TIME1970
+#        print '\tTime=%s' % time.ctime(t)
+
+def sntp_info(server):
+    client = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+    client.settimeout(0.5)
+    data = '\x1b' + 47 * '\0'
+    try:
+        client.sendto( data, (server, 123 ))
+    except:
+        return None, None
+    data, address = client.recvfrom( 1024 )
+    if data:
+        return struct.unpack( '!12I', data )[10], address
+
+def currenttime():
+    TIME1970 = 2208988800L
+    servers = ["{0}.pool.ntp.org".format(i) for i in xrange(5,4,-1)]
+    for s in servers:
+        ct,address = 0,0
+        try:
+            ct,address = sntp_info(s)
+        finally:
+            if ct:
+                return ct - TIME1970,address
+            
+        
+print "Success"
 
